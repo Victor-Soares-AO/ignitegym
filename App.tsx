4615f425f44cef122daa9f9e2d6fe05a20696b90
Victor Soares <victorsoaresao@gmail.com>
2024-03-19 10:04:54 +0100

    :tada: primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
+import {
+    useFonts,
+    Roboto_400Regular,
+    Roboto_700Bold
+} from '@expo-google-fonts/roboto';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: '#202024'
+            }}
+        >
+            <StatusBar
+                barStyle='light-content'
+                translucent
+                backgroundColor='transparent'
+            />
+
+            {
+                fontsLoaded
+                    ? <Text
+                        style={{ 
+                            color: '#FFF',
+                            fontSize: 18,
+                            fontWeight: '700'
+                        }}
+                    >
+                        IGNITE GYM
+                    </Text>
+                    : <View />
+            }
+        </View>
+    );
+}
